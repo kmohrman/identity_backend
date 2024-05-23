c092b1207edc7c647f98bbaf5bb3088d68796ab5
Kelci Mohrman <kelci.mohrman@cern.ch>
2024-05-23 05:51:07 -0400

    We dont need tbb for now

--- src/identity.cc
+++ src/identity.cc
@@ -27,7 +27,7 @@
 #include <memory>
 #include <thread>
 #include "triton/backend/backend_common.h"
-#include <tbb/task_scheduler_init.h>
+//#include <tbb/task_scheduler_init.h>
 #include "../SDL/LST.h"
 
 namespace triton { namespace backend { namespace identity {
@@ -610,7 +610,7 @@ TRITONBACKEND_ModelInstanceExecute(
   // For simplicity we just process each request separately... in
   // general a backend should try to operate on the entire batch of
   // requests at the same time for improved performance.
-  tbb::task_scheduler_init tsi(1);
+  //tbb::task_scheduler_init tsi(1);
   for (uint32_t r = 0; r < request_count; ++r) {
     TRITONBACKEND_Request* request = requests[r];
 
